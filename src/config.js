c36aa761441384eafed61d30d9ceb86c59c0942f
stropitek <kostro.d@gmail.com>
2015-08-12 12:32:22 -0500

    check config validity Throw error when not valid

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 var config = require('../config.json');
 var _ = require('lodash');
 var modConfig = [];
+var mandatory = ['interval', 'chunkSize', 'incrCol', 'database', 'table'];
 for (var i = 0; i < config.length; i++) {
     for (var j = 0; j < config[i].urls.length; j++) {
         var url = config[i].urls[j];
@@ -10,6 +11,14 @@ for (var i = 0; i < config.length; i++) {
         var last = modConfig[modConfig.length-1];
         last.url = url;
         delete  last.urls;
+    }
+}
+
+for (i = 0; i < modConfig.length; i++) {
+    var c = modConfig[i];
+    for (j = 0; j < mandatory.length; j++) {
+        var m = mandatory[j];
+        if(c[m] === undefined) throw new Error('Config error: ' + m + ' is mandatory');
     }
 }
 
