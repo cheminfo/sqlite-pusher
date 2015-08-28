dba15287f8378965206a62fbd9c5ba06b9fa7c2e
stropitek <kostro.d@gmail.com>
2015-08-27 19:14:26 -0500

    More meaningful debug prints

--- src/pusher.js
+++ src/pusher.js
@@ -7,6 +7,7 @@ var superagent = require('superagent');
 
 function Pusher(options) {
     this.options = options;
+    this.name = /[^\/]*$/.exec(this.options.database)[0];
     this.last = Promise.resolve();
     this.queueLength = 0;
     this.incr = 0;
@@ -16,11 +17,11 @@ function Pusher(options) {
 Pusher.prototype.start = function () {
     var that = this;
     if (this._started) {
-        debug('Cannot start, already started');
+        that._log('Cannot start, already started');
         return;
     }
     // open database
-    debug('opening database', this.options.database);
+    that._log('opening database');
     var db = new sqlite.cached.Database(this.options.database);
     this.database = new PromiseWrapper(db, ['all', 'run', 'get']);
     this._queue(function () {
@@ -31,10 +32,10 @@ Pusher.prototype.start = function () {
 
     this._interval = setInterval(function () {
         if (that.queueLength === 0 && !that.isReset) {
-            debug('interval: push');
+            that._log('interval: push');
             that._push();
         } else {
-            debug('interval: reset');
+            that._log('interval: reset');
             that._reset();
         }
     }, that.options.interval);
@@ -46,17 +47,19 @@ Pusher.prototype._reset = function () {
     this._push();
 };
 
+Pusher.prototype._log = function (msg) {
+    debug(this.name + ': ' + msg);
+};
+
 Pusher.prototype._queue = function (fn) {
     var that = this;
     this.queueLength++;
     this.last = this.last.then(fn).then(function (arg) {
         that.queueLength--;
-        debug('queue element succeeded. New q length: ' + that.queueLength);
         return arg;
     }, function (arg) {
-        debug('queue element failed' + arg);
+        that._log('queue element failed' + arg);
         that.queueLength--;
-        debug('New queue length: ' + that.queueLength);
         return arg;
     });
 };
@@ -65,27 +68,32 @@ Pusher.prototype._push = function () {
     var that = this;
     this._queue(function () {
         var query = 'select * from ' + that.options.table + ' where ' + that.options.incrCol + '>' + that.incr + ' order by ' + that.options.incrCol + ' ASC limit ' + that.options.chunkSize;
-        debug(query);
+        that._log(query);
         return that.database.all(query).then(function (r) {
             var incr;
             if (r.length) {
+                that._log('send ' + r.length + ' elements');
                 incr = r[r.length - 1][that.options.incrCol];
             } else {
+                that._log('no element to send');
                 return;
             }
 
             return new Promise(function (resolve, reject) {
+
                 superagent.put(that.options.pushUrl)
                     .send(r)
                     .end(function (err, res) {
                         if (err) return reject(err);
                         if (res.status === 200) {
                             that.incr = incr;
+                            that._log('incr ' + incr);
                             that._push();
                             that.isReset = false;
                             return resolve();
                         }
                         that.isReset = true;
+                        console.log(res);
                         return reject(res);
                     });
             });
@@ -95,25 +103,27 @@ Pusher.prototype._push = function () {
 
 Pusher.prototype._getLast = function () {
     var that = this;
-    debug('get ' + that.options.lastUrl);
+    that._log('get last from remote database');
     return new Promise(function (resolve, reject) {
         superagent.get(that.options.lastUrl)
             .end(function (err, res) {
                 if (err) {
-                    debug('Error with last entry url, check your configuration');
+                    that._log('Error with last entry url, check your configuration');
                     return reject(err);
                 }
                 if (res.status === 200) {
                     var incr = res.body[that.options.incrCol];
                     if (incr === undefined) {
-                        debug('Could not get increment');
+                        that._log('Could not get increment');
                         return reject(new Error('Could not get last increment'));
                     }
                     that.incr = incr;
+                    that._log('incr ' + incr);
                     return resolve();
                 } else if (res.status === 404) {
-                    debug('remote table does not exist');
+                    that._log('404 returned, remote table does not exist');
                     that.incr = 0;
+                    that._log('incr 0');
                     return resolve();
                 } else {
                     return reject(new Error('Unknown error'));
